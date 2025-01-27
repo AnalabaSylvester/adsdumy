4f520c59b5359fdbbea9acb4416308ad94de2450
ubachukwusylvester8@gmail.com <analaba67>
2025-01-26 16:46:25 -0800

    first commit 2

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ const ads = [
     // id: 1,
     videoUrl:
       'https://videocdn.cdnpk.net/videos/7ec32d3a-7baf-5e1b-b096-98539283e277/horizontal/previews/watermarked/large.mp4',
-    interval: 300 // 5 minutes in seconds
+    interval: 60 // 1 minutes in seconds
   },
   {
     // id: 2,
     videoUrl:
       'https://videocdn.cdnpk.net/videos/102bee2e-ffc2-5eb1-914f-445a73c63f28/horizontal/previews/watermarked/large.mp4',
-    interval: 300 // 5 minutes in seconds
+    interval: 60 // 1 minutes in seconds
   },
   {
     // id: 3,
     videoUrl:
       'https://videocdn.cdnpk.net/videos/a5f9c60f-7cc3-417a-9bd5-c0bffd7ce210/horizontal/previews/watermarked/large.mp4',
-    interval: 300 // 5 minutes in seconds
+    interval: 60 // 5 minutes in seconds
   }
 ]
 
